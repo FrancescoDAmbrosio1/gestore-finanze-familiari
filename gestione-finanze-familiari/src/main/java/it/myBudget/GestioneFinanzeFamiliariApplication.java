a4b7a339d5a62ab820065571809a0bfe0ce7ad9d
FrancescoDAmbrosio1 <158843577+FrancescoDAmbrosio1@users.noreply.github.com>
2024-10-16 22:42:26 +0200

    Add project

---
+++ gestione-finanze-familiari/src/main/java/it/myBudget/GestioneFinanzeFamiliariApplication.java
@@ -0,0 +1,13 @@
+package it.myBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneFinanzeFamiliariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneFinanzeFamiliariApplication.class, args);
+	}
+
+}
---
+++ gestione-finanze-familiari/src/test/java/it/myBudget/GestioneFinanzeFamiliariApplicationTests.java
@@ -0,0 +1,13 @@
+package it.myBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneFinanzeFamiliariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
